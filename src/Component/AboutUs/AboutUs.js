6911ba372a43f226cfccb1bb253fef30c57daded
ashishmaurya-crypto <ashishmaurya7547@gmail.com>
2024-09-05 00:39:23 +0530

    add code

--- src/Component/AboutUs/AboutUs.js
+++ src/Component/AboutUs/AboutUs.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import { Assest } from '../../ReusableComponent/Assest/Assest';
+import { useLocation } from 'react-router-dom';
 import './AboutUs.scss';
 
 export default function AboutUs() {
+  const location = useLocation();
+  const currentUrl = `${location.pathname}`;
+  
+  console.log("Current URL:", location);
 
   return (
     <>
       <Container fluid className='aboutus-container'>
         <Row className='aboutus-title'>
           <Col className='d-flex justify-content-start'>
-            <h4>About Us</h4>
+            <h4>{currentUrl.replace('/', '').toUpperCase()}</h4>
           </Col>
         </Row>
       </Container>
-
-
     </>
-  )
+  );
 }
